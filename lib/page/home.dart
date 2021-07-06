50b8e824a71ee624af381bb4dcac63bb9c366553
Ohgyuchan <gyu3637@gmail.com>
2021-07-06 23:56:30 +0900

    데이터 연결

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:intl/intl.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,32 +10,209 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<Map<String, String>> data = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    data = [
+      {
+        "image": "assets/images/ara-1.jpg",
+        "title": "네메시스 축구화275",
+        "location": "팡주팡역시 서구 화정동",
+        "price": "30,000",
+        "likes": "2"
+      },
+      {
+        "image": "assets/images/ara-2.jpg",
+        "title": "LA갈비 5kg팔아요~",
+        "location": "팡주팡역시 서구 풍암동",
+        "price": "100,000",
+        "likes": "5"
+      },
+      {
+        "image": "assets/images/ara-3.jpg",
+        "title": "치약팝니다",
+        "location": "팡주팡역시 서구 봉선동",
+        "price": "5,000",
+        "likes": "0"
+      },
+      {
+        "image": "assets/images/ara-4.jpg",
+        "title": "[풀박스]맥북프로16인치 터치바 스페이스그레이 256GB",
+        "location": "팡주팡역시 서구 임암동",
+        "price": "2,500,000",
+        "likes": "6"
+      },
+      {
+        "image": "assets/images/ara-5.jpg",
+        "title": "디월트존기임팩",
+        "location": "팡주팡역시 광산구 신촌동",
+        "price": "150,000",
+        "likes": "2"
+      },
+      {
+        "image": "assets/images/ara-6.jpg",
+        "title": "갤럭시s10",
+        "location": "팡주팡역시 서구 화정동",
+        "price": "180,000",
+        "likes": "2"
+      },
+      {
+        "image": "assets/images/ara-7.jpg",
+        "title": "선반",
+        "location": "팡주팡역시 서구 화정동",
+        "price": "15,000",
+        "likes": "2"
+      },
+      {
+        "image": "assets/images/ara-8.jpg",
+        "title": "냉장 쇼케이스",
+        "location": "팡주팡역시 북구 용봉동",
+        "price": "80,000",
+        "likes": "3"
+      },
+      {
+        "image": "assets/images/ara-9.jpg",
+        "title": "대우 미니냉장고",
+        "location": "팡주팡역시 서구 화정동",
+        "price": "30,000",
+        "likes": "3"
+      },
+      {
+        "image": "assets/images/ara-10.jpg",
+        "title": "멜킨스 풀업 턱걸이 판매합니다.",
+        "location": "팡주팡역시 서구 풍암동",
+        "price": "50,000",
+        "likes": "7"
+      },
+    ];
+  }
+
+  AppBar _appBarWidget() {
+    return AppBar(
+      title: GestureDetector(
+        onTap: () {
+          print('click');
+        },
+        onLongPress: () {
+          print('long pressed');
+        },
+        child: Row(
+          children: [
+            Text(
+              '아라동',
+            ),
+            Icon(Icons.arrow_drop_down)
+          ],
+        ),
+      ),
+      elevation: 1,
+      actions: [
+        IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+        IconButton(onPressed: () {}, icon: Icon(Icons.tune)),
+        IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              'assets/svg/bell.svg',
+              width: 22,
+            )),
+      ],
+    );
+  }
+
+  final oCcy = new NumberFormat("#,###", "ko_KR");
+  String calcStringToWon(String priceString) {
+    return "${oCcy.format(int.parse(priceString))} 원";
+  }
+
+  Widget bodyWidget() {
+    return ListView.separated(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      itemBuilder: (BuildContext _context, int index) {
+        return Container(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          height: 100,
+          child: Row(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.all(
+                  Radius.circular((10)),
+                ),
+                child: Image.asset(
+                  "${data[index]["image"]}",
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.only(left: 20),
+                  height: 100,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "${data[index]["title"]}",
+                        style: TextStyle(fontSize: 15),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "${data[index]["location"]}",
+                        style: TextStyle(
+                          fontSize: 13,
+                          color: Colors.black.withOpacity(0.3),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "${data[index]["price"]} 원",
+                        style: TextStyle(fontWeight: FontWeight.w500),
+                      ),
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            SvgPicture.asset(
+                              "assets/svg/heart_off.svg",
+                              height: 13,
+                              width: 13,
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text("${data[index]["likes"]}"),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+      separatorBuilder: (BuildContext _context, int index) {
+        return Container(
+          height: 1,
+          color: Colors.black.withOpacity(0.4),
+        );
+      },
+      itemCount: 10,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: GestureDetector(
-          onTap: () {
-            print('click');
-          },
-          onLongPress: () {
-            print('long pressed');
-          },
-          child: Row(
-            children: [
-              Text(
-                '아라동',
-              ),
-              Icon(Icons.arrow_drop_down)
-            ],
-          ),
-        ),
-        actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.tune)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.tune)),
-        ],
-      ),
+      appBar: _appBarWidget(),
+      body: bodyWidget(),
     );
   }
 }
