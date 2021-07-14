fee1d1e1608478d7033bc95b02df40a6434861ec
Ohgyuchan <gyu3637@gmail.com>
2021-07-14 16:42:07 +0900

    Change app title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'Carrot Clone',
       theme: ThemeData(
         primaryColor: Colors.white,
         primarySwatch: Colors.blue,
